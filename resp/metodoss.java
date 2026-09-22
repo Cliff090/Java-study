1f6c0b8dae6b5cae98ecdab88d7cf0309ddc9a8f
João Paulo <ketsuo110@gmail.com>
2026-09-21 21:14:28 -0300

    Atividades dia 21/09

---
+++ resp/metodoss.java
@@ -0,0 +1,42 @@
+public class metodoss{
+
+
+    public void doisnum(double a, double b) {
+        if (a<b){
+            System.out.printf("O numero maior é: %.2f%", b)
+
+        }
+        else{
+            System.out.printf("O numero maior é: %.2f%", a)
+        }
+    }
+
+    public void status(double a){
+        if (a>0){
+          System.out.println("o numero é positivo")
+        }else if (a<0){
+            System.out.println("o numero é negativo")
+        }else{
+            System.out.println("o numero é nulo/zero")
+        }
+    }
+    public void nota(double a){
+        if (a>=7.0){
+            System.out.printf("%.2f% aprovado!",a)
+        }else if (a<=6,9 && a>=5.0){
+            System.out.printf("%.2f% recuperação", a)
+        }else{
+            System.out.printf("%.2f% reprovado", a)
+        }
+    }
+    public void shop(double compra){
+        if (compra>=100){
+            valorfinal = compra*0.10
+            System.out.prinf("Valor inicial: %.2f%", compra)
+            System.out.prinf("Valor com desconto: %2f%", valorfinal)
+        }else{
+            System.out.printf("Valor da compra: %.2f% ", compra)
+        }
+    }
+    public void
+}
